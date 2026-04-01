d63cda00308f5bb12626bece63094938e27e6ed6
num876 <maliknumayr@gmail.com>
2026-04-01 20:58:37 +0100

    chore: update base path and prepare for push to new repo (99888)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/precision-power99/',
+  base: '/precision-power99888/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
